06db556c8e8d43400c8c665eec7572af07b4e6c1
Simon <simonbustamante21@gmail.com>
2023-06-08 23:51:12 -0400

    Pruebas en hooks useUiStore y useAuthStore, y reparacion de codigo en componente de useUiStore

---
+++ tests/hooks/useAuthStore.test.js
@@ -0,0 +1,177 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "../../src/store";
+import { useAuthStore } from "../../src/hooks";
+import { Provider } from "react-redux";
+import { act, renderHook, waitFor } from "@testing-library/react";
+import { initialState, notAuthenticatedState } from "../fixtures/authStates";
+import { testUserCredentials } from "../fixtures/testUser";
+import calendarApi from "../../src/api/calendarApi";
+
+
+
+const getMockStore = ( initialState ) => {
+    return configureStore({
+        reducer: {
+            // ui: uiSlice.reducer
+            auth: authSlice.reducer
+        },
+        preloadedState: {
+            // ui: { ...initialState }
+            auth: { ...initialState }
+        }
+    });
+}
+
+describe('Pruebas en el useAuthStore', () => {
+
+    beforeEach(
+        () => localStorage.clear()
+    );
+
+    test('debe de regresar los valores por defecto', () => {
+
+        const mockStore = getMockStore({ ...initialState });
+
+        const { result } = renderHook(
+            () => useAuthStore(), {
+                wrapper: ({ children }) => <Provider store={ mockStore } > { children } </Provider>
+            }
+        );
+
+        expect( result.current ).toEqual({
+            errorMessage: undefined,
+            status: 'checking',
+            user: {},
+            startLogin: expect.any( Function ),
+            startRegister: expect.any( Function ),
+            checkAuthToken: expect.any( Function ),
+            startLogout: expect.any( Function )
+        });
+
+    });
+    
+    test('startLogin debe de realizar el login correctamente', async() => {
+
+        const mockStore = getMockStore({ ...notAuthenticatedState });
+        const { result } = renderHook(
+            () => useAuthStore(), {
+                wrapper: ({ children }) => <Provider store={ mockStore } > { children } </Provider>
+            }
+        );
+
+        await act( async() => {
+            await result.current.startLogin( testUserCredentials )
+        });
+
+        const { errorMessage, status, user } = result.current;
+        expect({ errorMessage, status, user }).toEqual({
+            errorMessage: undefined,
+            status: 'authenticated',
+            user: {
+                name: 'TestSuit',
+                uid: '647fdbff2ee7f838105dca61'
+            }
+        });
+        expect( localStorage.getItem('token') ).toEqual( expect.any( String ) );
+        expect( localStorage.getItem('token-init-date') ).toEqual( expect.any( String ) );
+    });
+
+    test('startRegister debe de fallar la autenticación', async() => {
+
+        const mockStore = getMockStore({ ...notAuthenticatedState });
+        const { result } = renderHook(
+            () => useAuthStore(), {
+                wrapper: ({ children }) => <Provider store={ mockStore } > { children } </Provider>
+            }
+        )
+        
+        await act( async() => {
+            await result.current.startLogin({
+                email: 'alguncorreodetest123@gmail.com',
+                password: '123123123'
+            })
+        });
+
+        const { errorMessage, status, user } = result.current;
+
+        expect( localStorage.getItem('token') ).toBe( null );
+        expect( { errorMessage, status, user } ).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage: 'Un usuario no existe con ese correo',
+        });
+
+        await waitFor(
+            () => expect( result.current.errorMessage ).toBe( undefined )
+        );
+
+    });
+
+    test('startRegister debe de crear un usuario', async() => {
+
+        const newUser = {
+            name: 'alguncorreodetest',
+            email: 'alguncorreodetest123@gmail.com',
+            password: '123123123'
+        };
+        
+        const mockStore = getMockStore({ ...notAuthenticatedState });
+        const { result } = renderHook(
+            () => useAuthStore(), {
+                wrapper: ({ children }) => <Provider store={ mockStore } > { children } </Provider>
+            }
+        );
+        
+        // Para evitar que se realice el POST a la BD, se usa el spy
+        const spy = jest.spyOn( calendarApi, 'post' ).mockReturnValue({
+            data: {
+                ok: true,
+                uid: "123123",
+                name: "Test User",
+                token: "ALHUN-TOKEN",
+            }
+        })
+        
+        await act( async() => {
+            await result.current.startRegister(newUser);
+        });
+
+        const { errorMessage, status, user } = result.current;
+        expect({ errorMessage, status, user }).toEqual({
+            status: 'authenticated',
+            user: {
+                uid: "123123",
+                name: "Test User",
+            },
+            errorMessage: undefined,
+        });
+
+        // Para eliminar el espia, y solo funcione en este test, destruye el espia
+        spy.mockRestore();
+    })
+
+    test('startRegister debe de fallar la creación ', async() => {
+
+        const mockStore = getMockStore({ ...notAuthenticatedState });
+        const { result } = renderHook(
+            () => useAuthStore(), {
+                wrapper: ({ children }) => <Provider store={ mockStore } > { children } </Provider>
+            }
+        );
+               
+        await act( async() => {
+            await result.current.startRegister(testUserCredentials);
+        });
+
+        const { errorMessage, status, user } = result.current;
+        expect({ errorMessage, status, user }).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage: 'Un usuario existe con ese correo',
+        });
+
+    })
+    
+    
+
+});
--- tests/hooks/useUiStore.test.js
+++ tests/hooks/useUiStore.test.js
@@ -57,7 +57,75 @@ describe('Pruebas en useUiStore', () => {
         );
         expect( result.current.isDateModalOpen ).toBeTruthy();
 
-    })
+    });
+
+    test('toggleDateModal debe de cambiar el estado respectivamente ', () => {
+       
+        const mockStore = getMockStore({ isDateModalOpen: true });
+
+        const { result } = renderHook(
+            () => useUiStore(), {
+                wrapper: ({ children }) => <Provider store={ mockStore } > { children } </Provider>
+            }
+        );
+
+        const { closeDateModal } = result.current;
+
+        act(
+            () => {
+                closeDateModal();
+            }
+        );
+        expect( result.current.isDateModalOpen ).toBeFalsy();
+    });
+
+    test('closeDateModal debe de colocar false en isDateModalOpen ', () => {
+       
+        const mockStore = getMockStore({ isDateModalOpen: true });
+
+        const { result } = renderHook(
+            () => useUiStore(), {
+                wrapper: ({ children }) => <Provider store={ mockStore } > { children } </Provider>
+            }
+        );
+
+        const { closeDateModal } = result.current;
+
+        act(
+            () => {
+                closeDateModal();
+            }
+        );
+        expect( result.current.isDateModalOpen ).toBeFalsy();
+    });
+
+    test('toggleDateModal debe de cambiar el estado respectivamente ', () => {
+       
+        const mockStore = getMockStore({ isDateModalOpen: true });
+
+        const { result } = renderHook(
+            () => useUiStore(), {
+                wrapper: ({ children }) => <Provider store={ mockStore } > { children } </Provider>
+            }
+        );
+
+        const { toggleDateModal } = result.current;
+
+        act(
+            () => {
+                toggleDateModal();
+            }
+        );
+        expect( result.current.isDateModalOpen ).toBeFalsy();
+
+        act(
+            () => {
+                result.current.toggleDateModal();
+            }
+        );
+        expect( result.current.isDateModalOpen ).toBeTruthy();
+    });
+    
     
     
 })
